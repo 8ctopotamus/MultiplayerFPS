040b94be0ce6c0f86d3e0bc43443b14d38a24ece
8ctopotamus <joshnaylor88@gmail.com>
2017-09-15 21:28:23 -0500

    06 shooting

--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -6,22 +6,46 @@ public class PlayerSetup : NetworkBehaviour {
 	[SerializeField]
 	Behaviour[] componentsToDisable;
 
+	[SerializeField]
+	string remoteLayerName = "RemotePlayer";
+
 	Camera sceneCamera;
 
 	void Start () {
+		// disable components that should only be
+		// active on the player that we control
 		if (!isLocalPlayer) {
-			for (int i = 0; i < componentsToDisable.Length; i++) {
-				componentsToDisable[i].enabled = false;
-			}
+			DisableComponents ();
+			AssignRemoteLayer ();
 		} else {
+			// we are the local player: disable the scene camera
 			sceneCamera = Camera.main;
 			if (sceneCamera != null) {
 				sceneCamera.gameObject.SetActive (false);
 			}
 		}
+
+		RegisterPlayer ();
 	}
 
+	void RegisterPlayer () {
+		string _ID = "Player " + GetComponent<NetworkIdentity>().netId;
+		transform.name = _ID;
+	}
+
+	void AssignRemoteLayer () {
+		gameObject.layer = LayerMask.NameToLayer (remoteLayerName);
+	}
+
+	void DisableComponents () {
+		for (int i = 0; i < componentsToDisable.Length; i++) {
+			componentsToDisable[i].enabled = false;
+		}
+	}
+
+	// when we are destroyed
 	void OnDisable () {
+		// re-enable the scene camera
 		if (sceneCamera != null) {
 			sceneCamera.gameObject.SetActive (true);
 		}
